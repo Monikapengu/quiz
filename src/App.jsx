4664c9b925b9f1c45b7f5bda00cdda991032943a
Monika Pengu <mpengu17@beder.edu.al>
2023-12-17 17:35:47 +0100

    Reactproject

---
+++ src/App.jsx
@@ -0,0 +1,190 @@
+import './app.css';
+import {useEffect, useState, useMemo} from 'react';
+import Trivia from './components/Trivia';
+import Timer from './components/Timer';
+import Start from './components/Start';
+
+function App() {
+  const [userName, setUserName] = useState(null);
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [stop, setStop] = useState(false);
+  const [earned, setEarned] = useState("$ 0");
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers:[
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+
+    {
+      id: 2,
+      question: "When did the website 'Facebook' lounch?",
+      answers:[
+        {
+          text: "2004",
+          correct: false,
+        },
+        {
+          text: "2005",
+          correct: true,
+        },
+        { 
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who play the role of the harry-poter in the movie?",
+      answers: [
+        {
+          text: "Johny Deap",
+          correct: false,
+        },
+        {
+          text: "Leonardo di Caprio",
+          correct: false,
+        },
+        {
+          text: "Danzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "What sort of animal is Walt Disney's Dumbo?",
+      answers: [
+        {
+          text: "Deer",
+          correct: false,
+        },
+        {
+          text: "Rabbit",
+          correct: false,
+        },
+        {
+          text: "Donkey",
+          correct: false,
+        },
+        {
+          text: "Elephant",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: "Queen Anne was the daughter of which English Monarch?",
+      answers: [
+        {
+          text: "James II",
+          correct: true,
+        },
+        {
+          text: "Henry VIII",
+          correct: false,
+        },
+        {
+          text: "Victoria",
+          correct: false,
+        },
+        {
+          text: "William 1",
+          correct: false,
+        },
+      ],
+    },
+  ];
+
+
+  const moneyPyramid = useMemo(()=>
+   [ 
+    {id:1, amount:"$ 100"},
+    {id:2, amount:"$ 200"},
+    {id:3, amount:"$ 300"},
+    {id:4, amount:"$ 500"},
+    {id:5, amount:"$ 1000"},
+    {id:6, amount:"$ 2000"},
+    {id:7, amount:"$ 4000"},
+    {id:8, amount:"$ 8000"},
+    {id:9, amount:"$ 16000"},
+    {id:10, amount:"$ 32000"},
+    {id:11, amount:"$ 64000"},
+    {id:12, amount:"$ 125000"},
+    {id:13, amount:"$ 250000"},
+    {id:14, amount:"$ 50000"},
+    {id:15, amount:"$ 100000"},
+  ].reverse(),
+ []); 
+  
+  useEffect(() => {
+    questionNumber >1 && setEarned(moneyPyramid.find(m=> m.id === questionNumber -1).amount);
+  }, [moneyPyramid, questionNumber])
+  return (
+    <div className="app">
+      {userName ? (
+        <>
+        <div className="main">
+        {stop ? <h1 className='endText'>You Earned: {earned}</h1> : ( 
+         <> 
+          <div className="top">
+         <div className="timer">
+          <Timer setStop={setStop} 
+          questionNumber={questionNumber}/>
+         </div>
+        </div>
+        <div className="bottom">
+        <Trivia 
+        data={data} 
+        setStop={setStop}
+        questionNumber={questionNumber}
+        setQuestionNumber={setQuestionNumber} />
+        </div>
+        </>
+        )}
+      </div>
+      <div className="pyramid">
+        <ul className='moneyList'>
+          {moneyPyramid.map((m) =>(
+            <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
+          <span className='moneyListItemNumber'>{m.id}</span>
+          <span className='moneyListItemAmount'>{m.amount}</span>
+          </li>
+          ))}
+        </ul>
+    </div>
+        </>
+      ) : <Start setUserName={setUserName} />}  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Start.jsx
@@ -0,0 +1,20 @@
+import {useRef} from 'react';
+
+export default function Start({setUserName}) {
+
+    const inputRef = useRef();
+    const handleClick =() =>{
+        inputRef.current.value && setUserName(inputRef.current.value);
+    };
+  return (
+    <div className="start">
+        <input
+         placeholder="Enter your Name"
+        className="startInput"
+        ref={inputRef} />
+        <button 
+        className="startButton" 
+        onClick={handleClick}>Start</button>
+    </div>
+  )
+}
